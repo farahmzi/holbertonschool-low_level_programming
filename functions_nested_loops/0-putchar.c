e1da24caed55ca24740b3fb0f1b0e32afb137296
farahmzi <farahalmzini@gmail.com>
2025-05-04 19:57:48 +0000

    Add: 0-putchar.c prints _putchar without spaces

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
-#include"main.h"
+#include "main.h"
 
 /**
-*main-Entry point
-*Return:Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 _putchar('_');
@@ -15,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
