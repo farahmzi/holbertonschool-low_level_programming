bc3ad863dbe276813fd3361da114e2cc0f0387cf
farahmzi <farahalmzini@gmail.com>
2025-05-04 15:03:55 +0000

    Add: 0-putchar program

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,17 +2,18 @@
 
 /**
 *main-Entry point
-*Return:Always 0
+*Return:Always 0 (Success)
 */
 int main(void)
 {
-char*s="_putchar";
-int i=0;
-while(s[i])
-{
-_putchar(s[i]);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return(0);
 }
