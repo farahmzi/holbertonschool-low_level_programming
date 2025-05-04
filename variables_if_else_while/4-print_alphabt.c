9bee5608bade81beb5baae0d925f331d9a8df812
farahmzi <farahalmzini@gmail.com>
2025-05-04 14:24:06 +0000

    Update: compressed version of 4-print_alphabt.c without spaces

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase except q and e
- *
- * Return: Always 0 (Success)
- */
+*main-Prints some letters of alphabet
+*
+*Return:Always(Success)
+*/
 int main(void)
 {
-int i;
-for(i=97;i<=122;i++)
+char c;
+for(c='a';c<='z';c++)
 {
-if(i!=101&&i!=113)
+if(c!='e'&&c!='q')
 {
-putchar(i);
+putchar(c);
 }
 }
 putchar('\n');
