8fade1cea36e1a6b881be7af9944b926625e26b8
farahmzi <farahalmzini@gmail.com>
2025-05-18 01:11:15 +0000

    add this

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - check the code
+*@argc:arguments count
+*@argv:the argument array of command string
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
