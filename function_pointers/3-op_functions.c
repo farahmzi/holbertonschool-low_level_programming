6931746d587cb15fb0a73b7c142ea35185b709ef
farahmzi <farahalmzini@gmail.com>
2025-05-31 18:54:26 +0000

    g

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+/**
+ * op_add - return sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - return difference
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - return product
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - return quotient
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - return remainder
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
