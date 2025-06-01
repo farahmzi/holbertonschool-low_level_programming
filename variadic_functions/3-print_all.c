648fc3f6abadd8849753c02bab41e28601d9bd4a
farahmzi <farahalmzini@gmail.com>
2025-06-01 16:06:38 +0000

    gg

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,41 +1,38 @@
-#includestdio.h>
-#includestdarg.h>
-#includevariadic_functions.h"
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * printll - prints anything
- * @form: list of types of arguments passed
+ * print_all - prints anything
+ * @format: list of types of arguments passed
  */
-void pri_all(const char * const format, ...)
+void print_all(const char * const format, ...)
 {
-va_list gs;
-unsignednt i = 0;
-char *st *sep = "";
-va_startrgs, format);
-هwhileformat && format[i])
+va_list args;
+unsigned int i = 0;
+char *str, *sep = "";
+va_start(args, format);
+while (format && format[i])
 {
-if (form[i] == 'c')
+if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
 {
-printf("%c", sep, va_arg(args, int));
+printf("%s", sep);
+if (format[i] == 'c')
+printf("%c", va_arg(args, int));
+if (format[i] == 'i')
+printf("%d", va_arg(args, int));
+if (format[i] == 'f')
+printf("%f", va_arg(args, double));
+if (format[i] == 's')
+{
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
 }
-if (form[i] == 'i')
-{
-printf("%d", sep, va_arg(args, int));
+sep = ", ";
 }
-if (form[i] == 'f')
-{
-printf("%f", sep, (float)va_arg(args, double));
-}
-if (form[i] == 's')
-{
-str = varg(args, char *);
-if (!str
-str = "(l)";
-printf("%s", sep, str);
-}
-if (form[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-sep = ",;
 i++;
 }
-printf("");
-va_end(as);
-}<Right>
+va_end(args);
+printf("\n");
+}
