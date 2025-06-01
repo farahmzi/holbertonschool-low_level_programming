315fd6282dda00ce69337e436764031ef6b689b8
farahmzi <farahalmzini@gmail.com>
2025-06-01 16:11:06 +0000

    gg

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,33 +3,39 @@
 #include <stdarg.h>
 /**
  * print_all - prints anything
- * @format: list of types of arguments passed
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
-unsigned int i = 0;
+unsigned int i = 0, j;
 char *str, *sep = "";
+struct printer {
+char type;
+void (*func)(va_list);
+} ops[] = {
+{'c', (void (*)(va_list))[](va_list a){ printf("%c", va_arg(a, int)); }},
+{'i', (void (*)(va_list))[](va_list a){ printf("%d", va_arg(a, int)); }},
+{'f', (void (*)(va_list))[](va_list a){ printf("%f", va_arg(a, double)); }},
+{'s', (void (*)(va_list))[](va_list a){
+char *s = va_arg(a, char *);
+printf("%s", s ? s : "(nil)");
+}},
+{0, NULL}
+};
 va_start(args, format);
 while (format && format[i])
 {
-if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+j = 0;
+while (ops[j].type)
+{
+if (format[i] == ops[j].type)
 {
 printf("%s", sep);
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));
-if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-if (format[i] == 's')
-{
-str = va_arg(args, char *);
-if (str == NULL)
-str = "(nil)";
-printf("%s", str);
-}
+ops[j].func(args);
 sep = ", ";
+}
+j++;
 }
 i++;
 }
