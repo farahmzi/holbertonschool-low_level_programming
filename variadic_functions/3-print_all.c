af6098d4e509452b75ca2fecdea35a2de91a0532
farahmzi <farahalmzini@gmail.com>
2025-06-01 16:19:50 +0000

    gg

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,43 +2,41 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * print_all - prints anything based on format
+ * @format: list of argument types
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
-unsigned int i = 0, j;
-char *str, *sep = "";
-struct printer {
-char type;
-void (*func)(va_list);
-} ops[] = {
-{'c', (void (*)(va_list))[](va_list a){ printf("%c", va_arg(a, int)); }},
-{'i', (void (*)(va_list))[](va_list a){ printf("%d", va_arg(a, int)); }},
-{'f', (void (*)(va_list))[](va_list a){ printf("%f", va_arg(a, double)); }},
-{'s', (void (*)(va_list))[](va_list a){
-char *s = va_arg(a, char *);
-printf("%s", s ? s : "(nil)");
-}},
-{0, NULL}
-};
+unsigned int i = 0;
+char *str, *separator = "";
 va_start(args, format);
 while (format && format[i])
 {
-j = 0;
-while (ops[j].type)
+switch (format[i])
 {
-if (format[i] == ops[j].type)
-{
-printf("%s", sep);
-ops[j].func(args);
-sep = ", ";
+case 'c':
+printf("%s%c", separator, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", separator, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", separator, va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", separator, str);
+break;
+default:
+i++;
+continue;
 }
-j++;
-}
+separator = ", ";
 i++;
 }
-va_end(args);
 printf("\n");
+va_end(args);
 }
