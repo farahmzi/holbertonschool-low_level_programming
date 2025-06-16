8d71e7a1f8c659b238b6c6838c88ef4edf8d505b
farahmzi <farahalmzini@gmail.com>
2025-06-16 14:58:48 +0000

    add new file double

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len - يحسب عدد العقد في لائحة dlistint_t
+ * @h: مؤشر لأول عقدة في اللائحة
+ * Return: عدد العناصر
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return count;
+}
