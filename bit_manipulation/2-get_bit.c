8d95d868cf643e366fb190e187f58858987a9500
farahmzi <farahalmzini@gmail.com>
2025-06-18 14:10:44 +0000

    binary representation

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*get_bit - Returns the value of a bit at a given index
+*@n: The number to extract the bit from
+*@index: The index of the bit (starting from 0)
+*Return: The value of the bit (0 or 1), or -1 if error
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+return (-1);
+return ((n >> index) & 1);
+}
