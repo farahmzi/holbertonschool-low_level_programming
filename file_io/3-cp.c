6f926c43d7e17ccf53715bf6eb031dc4526afa31
farahmzi <farahalmzini@gmail.com>
2025-06-18 16:35:29 +0000

    Fix error code for read failure

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,44 +20,48 @@ exit(100);
 }
 
 /**
- * main - Copies content from file_from to file_to
- * @ac: Argument count
- * @av: Argument vector
- * Return: 0 on success
+ * open_files - Opens source and destination files
+ * @file_from: Source file name
+ * @file_to: Destination file name
+ * @fd_from: Pointer to source file descriptor
+ * @fd_to: Pointer to destination file descriptor
  */
-int main(int ac, char **av)
+void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
 {
-int fd_from, fd_to;
-ssize_t r, w;
-char buffer[BUF_SIZE];
-
-if (ac != 3)
+*fd_from = open(file_from, O_RDONLY);
+if (*fd_from == -1)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-
-fd_from = open(av[1], O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 exit(98);
 }
 
-fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1)
+*fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (*fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-close_fd(fd_from);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+close_fd(*fd_from);
 exit(99);
 }
+}
+
+/**
+ * copy_file - Copies content from source to destination
+ * @fd_from: Source file descriptor
+ * @fd_to: Destination file descriptor
+ * @file_from: Source file name
+ * @file_to: Destination file name
+ */
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+ssize_t r, w;
+char buffer[BUF_SIZE];
 
 while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
 {
 w = write(fd_to, buffer, r);
 if (w != r)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 close_fd(fd_from);
 close_fd(fd_to);
 exit(99);
@@ -66,14 +70,34 @@ exit(99);
 
 if (r == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 close_fd(fd_from);
 close_fd(fd_to);
 exit(98);
 }
+}
 
+/**
+ * main - Entry point
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+int fd_from, fd_to;
+
+if (ac != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+open_files(av[1], av[2], &fd_from, &fd_to);
+copy_file(fd_from, fd_to, av[1], av[2]);
 close_fd(fd_from);
 close_fd(fd_to);
+
 return (0);
 }
 
