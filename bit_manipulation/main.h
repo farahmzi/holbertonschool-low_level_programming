051c4181f6a2d45062f9e96733e8cb05c1be15c9
farahmzi <farahalmzini@gmail.com>
2025-06-18 13:59:12 +0000

    New files added

---
+++ bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Includes */
+#include <stddef.h>  /* For NULL */
+#include <stdio.h>   /* Optional: if needed for testing */
+
+/* Prototype */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
