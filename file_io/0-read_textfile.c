7757f8ffce38bc4480d90cc32b90209f2f32cd76
farahmzi <farahalmzini@gmail.com>
2025-06-18 15:55:58 +0000

    Add read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters it should read and print
+ *
+ * Return: The actual number of letters it could read and print
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
